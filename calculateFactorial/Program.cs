850223ffa107367de708f25490e37eeb0dacd25b
Luis Diego Brenes Loaiza <l.brenesluis@gmail.com>
2022-04-05 23:52:27 -0600

    Create app factorial

--- calculateFactorial/Program.cs
+++ calculateFactorial/Program.cs
@@ -6,7 +6,27 @@ namespace calculateFactorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number;
+            int factorial = 1;
+
+            Console.WriteLine("***************************");
+            Console.WriteLine("    Factorial Number App   ");
+            Console.WriteLine("***************************");
+            Console.WriteLine("");            
+            Console.WriteLine("Enter the number to execute");
+            Console.WriteLine("");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                factorial *= i;
+                Console.WriteLine("Factorial of {0}! * {1} = {2}\n", number, i, factorial);
+            }
+            
+
+            Console.WriteLine("The factorial of " + number + " results in " + factorial);
+            Console.ReadLine();
+
         }
     }
 }
